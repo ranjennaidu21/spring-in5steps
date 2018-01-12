2b8f986f1f2a90f7a041f096ee0e0fca279d18a3
Anonymous <Anonymous@Anonymous-PC>
2018-01-12 22:32:55 +0800

    Autowiring in Depth - by Name and @Primary

--- spring-in5steps/src/main/java/com/ranjen/spring/basics/springin5steps/BinarySearchImpl.java
+++ spring-in5steps/src/main/java/com/ranjen/spring/basics/springin5steps/BinarySearchImpl.java
@@ -7,12 +7,17 @@ import org.springframework.stereotype.Component;
 public class BinarySearchImpl {
 
 	@Autowired
-	private SortAlgorithm sortAlgorithm;
+	//if there are two bean implementing same interface, we can choose which bean by specifying the bean name as below(refactor: eg change
+	// quickSortAlgorithm to bubbleSortAlgorithm)
+	private SortAlgorithm quickSortAlgorithm;
+	
+	//or add @Primary in the bean class which have higher precedence regardless of name mentioned here. 
+	//private SortAlgorithm sortAlgorithm;
 	
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 
-		int[] sortedNumbers = sortAlgorithm.sort(numbers);
-		System.out.println(sortAlgorithm);
+		int[] sortedNumbers = quickSortAlgorithm.sort(numbers);
+		System.out.println(quickSortAlgorithm);
 		// Search the array
 		return 3;
 	}
--- spring-in5steps/src/main/java/com/ranjen/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ spring-in5steps/src/main/java/com/ranjen/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,10 +1,12 @@
 package com.ranjen.spring.basics.springin5steps;
 
-import org.springframework.context.annotation.Primary;
+//import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+//@Primary
+//if you put Primary here , when interface SortAlgorithm is injected to the BinarySearchImpl , it will take this as bean 
+//to be injected instead of the QuickSortAlgorithm bean.
 public class BubbleSortAlgorithm implements SortAlgorithm {
 	public int[] sort(int[] numbers) {
 		// Logic for Bubble Sort
