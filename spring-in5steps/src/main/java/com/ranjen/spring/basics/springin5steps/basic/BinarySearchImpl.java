13663fbb26487302ec54bf44f476e31fed404f71
Anonymous <Anonymous@Anonymous-PC>
2018-01-13 15:29:27 +0800

    LifeCycle of Bean - PostConstruct and PreDestroy annotation

--- spring-in5steps/src/main/java/com/ranjen/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ spring-in5steps/src/main/java/com/ranjen/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.ranjen.spring.basics.springin5steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -10,7 +15,8 @@ import org.springframework.stereotype.Component;
 //BS: to check scope of the bean
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
-
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	//if there are two bean implementing same interface, we can choose which bean by 3 ways (AM-Autowiring ways)
 	//AM-1: specifying the bean name as below(refactor: eg change
@@ -30,6 +36,26 @@ public class BinarySearchImpl {
 		System.out.println(sortAlgorithm);
 		// Search the array
 		return 3;
+	}
+	
+	@PostConstruct
+	//check in log , this method is called once beans have been and all it dependency have benn initialized 
+	public void postConstruct() {
+		logger.info("postConstruct");
+	}
+
+	@PreDestroy
+	//check in log , this method is called once beans is removed or leave from the Spring IOC Application context after run the called method.
+	//cannot see in the log for prototype bean because:
+/*	In contrast to the other scopes, Spring does not manage the complete lifecycle of a prototype bean: 
+	the container instantiates, configures, and otherwise assembles a prototype object, and hands it to the client, 
+	with no further record of that prototype instance. Thus, although initialization lifecycle callback methods are called on all objects 
+	regardless of scope, in the case of prototypes, configured destruction lifecycle callbacks are not called. 
+	The client code must clean up prototype-scoped objects and release expensive resources that the prototype bean(s) are holding. 
+	To get the Spring container to release resources held by prototype-scoped beans, try using a custom bean post-processor, 
+	which holds a reference to beans that need to be cleaned up.*/
+	public void preDestroy() {
+		logger.info("preDestroy");
 	}
 
 }
